3b451fe1188f3eb220010e20886d4da0be160b01
lgreitis <lukas.greicius@ktu.edu>
2023-04-14 21:13:12 +0300

    fix: missing env

--- src/constants.ts
+++ src/constants.ts
@@ -14,6 +14,7 @@ if (
   throw new Error("Bad .env");
 }
 
+export const SECRET_KEY = process.env.SECRET_KEY;
 export const FETCH_PROXY_STRING = process.env.FETCH_PROXY_STRING;
 export const INVENTORY_PROXY_STRING = process.env.INVENTORY_PROXY_STRING;
 export const APIS_KEY = process.env.APIS_KEY;
