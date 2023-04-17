20d8b05d484ea441df8db21b8ccbcd55ef12148d
lgreitis <lukas.greicius@ktu.edu>
2023-04-17 04:05:26 +0300

    feat: check other people's inventory

--- src/bot/commands/inventoryValue.ts
+++ src/bot/commands/inventoryValue.ts
@@ -6,18 +6,68 @@ import { SlashCommand } from "src/discordTypes";
 export const inventoryValue: SlashCommand = {
   command: new SlashCommandBuilder()
     .setName("value")
+    .addMentionableOption((option) => {
+      return option
+        .setName("user")
+        .setDescription("User that you want to check inventory of")
+        .setRequired(false);
+    })
     .setDescription("Shows your inventory value"),
   execute: async (interaction) => {
-    const user = await prisma.user.findFirst({
+    const invokingUser = await prisma.user.findFirst({
       where: {
         accounts: {
           some: { provider: "discord", providerAccountId: interaction.user.id },
+        },
+      },
+    });
+
+    if (!invokingUser) {
+      interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setAuthor({ name: "Plutus" })
+            .setDescription(
+              `We can't seem to find you on plutus.lukasgreicius.com, make sure that you're registered before using this command.`
+            ),
+        ],
+      });
+
+      return;
+    }
+
+    const mentionedUserUsername =
+      interaction.options.get("user")?.user?.username;
+
+    const mentionedUser = interaction.options.get("user")?.value?.toString();
+
+    const user = await prisma.user.findFirst({
+      where: {
+        accounts: {
+          some: {
+            provider: "discord",
+            providerAccountId: mentionedUser ?? interaction.user.id,
+          },
         },
       },
       include: {
         Inventory: { include: { UserItem: { include: { Item: true } } } },
       },
     });
+
+    if ((mentionedUser && !user) || (mentionedUser && !user?.public)) {
+      interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setAuthor({ name: "Plutus" })
+            .setDescription(
+              `User not found or has set inventory visibility to private.`
+            ),
+        ],
+      });
+
+      return;
+    }
 
     if (!user || !user.Inventory) {
       interaction.reply({
@@ -55,7 +105,11 @@ export const inventoryValue: SlashCommand = {
       embeds: [
         new EmbedBuilder()
           .setAuthor({ name: "Plutus" })
-          .setTitle(`${interaction.user.username} inventory value:`)
+          .setTitle(
+            `${
+              mentionedUserUsername ?? interaction.user.username
+            } inventory value:`
+          )
           .setFields([
             {
               name: "Invested",
