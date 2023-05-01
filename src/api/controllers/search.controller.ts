4189bf683111cc1b7ca63a5353bdce781e0dc0de
lgreitis <lukas.greicius@ktu.edu>
2023-05-01 18:05:14 +0300

    feat: optional id return

--- src/api/controllers/search.controller.ts
+++ src/api/controllers/search.controller.ts
@@ -7,6 +7,7 @@ import { sharedSecretHeaderHook } from "src/utils/sharedSecretHook";
 const searchQueryString = Type.Object({
   searchString: Type.String(),
   returnMatchData: Type.Boolean({ default: false }),
+  returnId: Type.Boolean({ default: false }),
 });
 
 type searchQueryStringType = Static<typeof searchQueryString>;
@@ -21,6 +22,46 @@ type searchResultType = Static<typeof searchResult>;
 let itemCache: { marketHashName: string; icon: string }[] = [];
 
 const searchController: FastifyPluginCallback = (fastify, options, done) => {
+  fastify.route<{
+    Querystring: searchQueryStringType;
+    Reply: searchResultType;
+  }>({
+    method: "GET",
+    url: "/search",
+    schema: {
+      description: "Fuzzy search items",
+      querystring: searchQueryString,
+      response: {
+        200: searchResult,
+      },
+    },
+    preHandler: sharedSecretHeaderHook,
+    handler: async (request, reply) => {
+      try {
+        // TODO: doesn't slow down too much but still need a better cache solution
+        const itemLength = await prisma.item.count();
+        if (itemCache.length === 0 || itemLength !== itemCache.length) {
+          itemCache = await prisma.item.findMany({
+            select: {
+              marketHashName: true,
+              icon: true,
+              id: request.query.returnId,
+            },
+          });
+        }
+        const result = search(request.query.searchString, itemCache, {
+          keySelector: (item) => item.marketHashName,
+          returnMatchData: request.query.returnMatchData,
+          threshold: 0.7,
+        });
+
+        reply.status(200).send({ success: true, result });
+      } catch {
+        reply.status(500).send({ success: false, result: [] });
+      }
+    },
+  });
+
   fastify.route<{
     Querystring: searchQueryStringType;
     Reply: searchResultType;
